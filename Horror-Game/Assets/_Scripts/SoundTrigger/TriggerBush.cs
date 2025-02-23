6c8c9ffc4535612e3fc3ac20fe15170e5dd7b651
Sam975320 <C23330671@mytudublin.ie>
2025-02-23 14:23:42 +0000

    testing audio
    changing it to networkBehaviour

--- Horror-Game/Assets/_Scripts/SoundTrigger/TriggerBush.cs
+++ Horror-Game/Assets/_Scripts/SoundTrigger/TriggerBush.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerBush : MonoBehaviour
+public class TriggerBush : NetworkBehaviour
 {
     public EnvironmentType environmentType = EnvironmentType.Bush;
 
