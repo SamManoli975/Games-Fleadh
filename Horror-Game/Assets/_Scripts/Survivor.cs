b2a4a445fcd1c95fe99dcee9b35e428ce3c25e93
Nojus Gaizauskas <nojusgaizauskas@gmail.com>
2025-02-28 10:33:48 +0000

    Revert all saturation changes
    Couldnt figure it out :(

--- Horror-Game/Assets/_Scripts/Survivor.cs
+++ Horror-Game/Assets/_Scripts/Survivor.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Inventory))]
 [RequireComponent(typeof(Clicker))]
@@ -12,7 +10,6 @@ using UnityEngine.Rendering;
 public class Survivor : NetworkBehaviour
 {
     public static Survivor instance;
-    
 
     UI_Inventory uI_Inventory;
     UI_HoveredMessage uI_HoveredMessage;
@@ -24,12 +21,6 @@ public class Survivor : NetworkBehaviour
     Hand hand;
     Health health;
     Movement movement;
-    private Monster monster; // Store the reference to the Monster
-    [SerializeField] private Volume postProcessingVolume; // Reference to Post-Processing Volume
-    private ColorAdjustments colorAdjustments; // Holds color adjustments settings
-
-    [SerializeField] private float maxDesaturationDistance = 10f; // Maximum distance where desaturation is applied
-    [SerializeField] private float minSaturation = -1f; // Minimum saturation value when killer is very close
 
     void Awake()
     {
@@ -45,8 +36,6 @@ public class Survivor : NetworkBehaviour
         hand = GetComponent<Hand>();
         health = GetComponent<Health>();
         movement = GetComponent<Movement>();
-        // Find Monster in the scene (fixes the error)
-        monster = FindObjectOfType<Monster>();
 
         if (IsOwner)
         {
@@ -71,32 +60,8 @@ public class Survivor : NetworkBehaviour
         {
             health.onDied.AddListener(HandleDeath);
         }
-
-        // Try to get the Color Adjustments component from the Post-Processing Volume
-        if (postProcessingVolume != null && postProcessingVolume.profile.TryGet(out colorAdjustments))
-        {
-            colorAdjustments.saturation.Override(0); // Set default saturation
-        }
-    }
-    void Update()
-    {
-        if (IsOwner)
-        {
-            UpdateDesaturation(); // Continuously update desaturation based on killer proximity
-        }
     }
 
-    void UpdateDesaturation()
-    {
-        if (monster == null || colorAdjustments == null)
-            return; // Exit if references are missing
-
-        float distanceToKiller = Vector3.Distance(transform.position, monster.transform.position); // Calculate distance to killer
-
-        float saturationValue = Mathf.Lerp(minSaturation, 0, distanceToKiller / maxDesaturationDistance); // Linearly interpolate saturation
-
-        colorAdjustments.saturation.Override(saturationValue); // Apply new saturation value
-    }
     void HandleDeath()
     {
         if (GameManager.instance != null)
